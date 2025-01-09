26e2f93d5609cefc06cf8240063dca562044ff40
Andy <163947665+chuckcrump@users.noreply.github.com>
2025-01-08 23:09:44 -0800

    Add sidebar adding conversation functionality

---
+++ src/components/Sidebar.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let conversations
+  export let newConvo
+  export let model
+  export let responses
+
+  function selectConversation(conversation) {
+    console.log(conversation)
+    model = conversation.currentModel
+    responses = conversation.responses
+  }
+</script>
+
+<div class="flex flex-col h-screen w-56 bg-gray-800 text-white fixed">
+  <h1>Hello!</h1>
+  <button onclick={newConvo}>Add</button>
+  {#each conversations as conversations}
+    <button onclick={() => selectConversation(conversations)}>{conversations.name}</button>
+  {/each}
+</div>
